ab6bcbc62d0b5684728d504de4cc737ecac2b7db
cry20011 <sergeynovik123@mail.ru>
2024-02-04 15:02:48 +0300

    Сделаны сервер и клиент для отправки и cчитывания курса

---
+++ client/src/main/java/ru/serg/client/currency_listeners/impl/RestCurrencyListener.java
@@ -0,0 +1,29 @@
+package ru.serg.client.currency_listeners.impl;
+
+import org.springframework.web.client.RestTemplate;
+import ru.serg.client.currency_listeners.CurrencyListener;
+
+import java.util.concurrent.TimeUnit;
+
+public class RestCurrencyListener implements CurrencyListener {
+    RestTemplate restTemplate = new RestTemplate();
+    private final String url;
+    private final int pollMillisInterval;
+
+    public RestCurrencyListener(String url, int pollMillisInterval) {
+        this.url = url;
+        this.pollMillisInterval = pollMillisInterval;
+    }
+
+    @Override
+    public void Listen() {
+        while (true) {
+            System.out.println(restTemplate.getForObject(url, String.class));
+            try {
+                Thread.sleep(pollMillisInterval);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
